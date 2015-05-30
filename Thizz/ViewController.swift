ab93173be77a77d033e770fafa1ae9f33c860716
Stan van Oers <stanvanoers@gmail.com>
2015-05-30 09:31:00 +0200

    Second time blanc build.

---
+++ Thizz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thizz
+//
+//  Created by Stan van Oers on 30/05/15.
+//  Copyright (c) 2015 Stan van Oers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ThizzTests/ThizzTests.swift
@@ -0,0 +1,36 @@
+//
+//  ThizzTests.swift
+//  ThizzTests
+//
+//  Created by Stan van Oers on 30/05/15.
+//  Copyright (c) 2015 Stan van Oers. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ThizzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
